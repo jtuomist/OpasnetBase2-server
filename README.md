e3662c21b2ac9782a4249e33cc069b24cae8d83c
trintala <teemu.rintala.a@gmail.com>
2019-07-26 20:42:21 +0300

    Update installation instructions

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # OpasnetBase2-server
 
+A web based application for cataloging and serving datasets. 
+
 ## Requirements
  - Web server
  - PHP
  - PHP Composer
  - MongoDB server
  - SQL server
+ 
+## Install
+
+*Note: these instructions may be out of date*
+1. Copy files to a secure location
+2. Ensure the files in the offline folder are not going to be accessible (.htaccess for Apache2 included)
+3. Modify user names, passwords/secrets and permissions in offline/establish.sql (these are required from interfacing applications)
+4. Create a new SQL database and import offline/establish.sql
+5. Create a new mongdodb database (probably don't need to initialize)
+6. Edit and rename config.dist.php to config.php
+7. Run PHP composer to download the php mongodb driver
+8. Copy files to a location accessible to the internet
--- config.dist.php
+++ config.dist.php
@@ -2,16 +2,16 @@
 
 $db_config = array(
 	'opasnet' => array(
-		'mongo_host' => 'localhost',
-		'mongo_db' => 'opasnet',
-		'mongo_username' => '',
-		'mongo_password' => '',
-		'mysql_host' => 'localhost',
-		'mysql_db' => 'opasnet',
-		'mysql_username' => 'opasnet_user',
-		'mysql_password' => '',
-		'username' => 'opasnet_user',
-		'password' => ''
+		'mongo_host' => 'localhost', // mongodb host
+		'mongo_db' => 'opasnet', // mongodb database name
+		'mongo_username' => '', // mongodb user name
+		'mongo_password' => '', // mongodb password
+		'mysql_host' => 'localhost', // SQL host
+		'mysql_db' => 'opasnet', // SQL database name
+		'mysql_username' => 'opasnet_user', // SQL user name
+		'mysql_password' => '', // SQL password
+		'username' => 'opasnet_user', // redundant?
+		'password' => '' // redundant?
 	)
 );
 	
